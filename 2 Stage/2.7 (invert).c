436d74096df65ca30f53690147300daf9e7a70f7
shadowik <zahar.mark@bk.ru>
2023-09-06 01:41:45 +0400

    2.7 task

---
+++ 2 Stage/2.7 (invert).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int invert(int x, int p, int n) {
+    int b = ((1 << (p + n)) - 1) & (~0 << p);
+//    printf("%b\n", b);
+//    printf("%b\n", ~(x & (~(~0 << n) << p)) & b);
+//    printf("%b\n", (x & (~0 << (p + n))) | ~(x & (~(~0 << n) << p)) & b | (x & ~(~0 << p)));
+    return (x & (~0 << (p + n))) | ~(x & (~(~0 << n) << p)) & b | (x & ~(~0 << p));
+}
+
+int main() {
+    printf("%b\n", invert(0b1111001010001111, 6, 4));
+    return 0;
+}
